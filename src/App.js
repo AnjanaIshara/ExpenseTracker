2cebff8990b22ece61f1a1ed3437fa3c505b9253
anjana ishara <anjanaishara08@gmail.com>
2021-05-03 22:15:51 +0530

    Expense tracke static

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import Expenses from './components/Expenses/Expenses';
 function App() {
+  const expenses = [
+    {
+      id: "e1",
+      title: "biscuit",
+      amount: 100,
+      date: new Date(2021, 4, 1),
+    },
+    {
+      id: "e2",
+      title: "fruits",
+      amount: 300,
+      date: new Date(2021, 3, 10),
+    },
+    {
+      id: "e3",
+      title: "jam",
+      amount: 100,
+      date: new Date(2021, 2, 17),
+    },
+    {
+      id: "e4",
+      title: "vegetaables",
+      amount: 1000,
+      date: new Date(2021, 3, 22),
+    },
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Let's get started</h2>
+      <Expenses items={expenses}/>
     </div>
   );
 }
